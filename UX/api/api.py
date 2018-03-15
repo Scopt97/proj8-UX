e36c5cb793683dbadba4e666846d6129da83403f
Kyle Nielsen <kylen@uoregon.edu>
2018-03-14 18:53:13 -0700

    Working as expected. Resources still expect token in header, rather than flask-based auth

--- UX/api/api.py
+++ UX/api/api.py
@@ -1,10 +1,10 @@
 # Credit: https://blog.miguelgrinberg.com/post/restful-authentication-with-flask for parts of the authentication implementation
 #TODO merge app.py (brevet calculator) with api ?
-#TODO jsonify results from resources ?
+#TODO jsonify results from resources ? Firefox recognizes it as json anyway
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, session
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 import os
@@ -13,18 +13,41 @@ from itsdangerous import (TimedJSONWebSignatureSerializer \
                                   as Serializer, BadSignature, \
                                   SignatureExpired)
 import time
-from flask_httpauth import HTTPBasicAuth
+#from flask_httpauth import HTTPBasicAuth
+import flask_login
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, validators, PasswordField
+
+from urllib.parse import urlparse, urljoin
+
 # Instantiate the app
 app = Flask(__name__)
 api = Api(app)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = ('/api/login')
 app.config['SECRET_KEY'] = 'Secure Phra_se'
-auth = HTTPBasicAuth()
+#auth = HTTPBasicAuth()
 
 client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
 db = client.tododb
-#userdb = client.userdb #TODO delete if below works
 users = db.userdb  # The users collection
 current_id = 0  # The unique id that will be given to the next user
+
+
+class User(flask_login.UserMixin):
+    def __init__(self, user_id):
+        self.id = str(user_id)#TODO.encode('utf-8').decode('utf-8')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[validators.DataRequired(message=u'Input required')])
+    password = PasswordField('Password', validators=[validators.DataRequired(message=u'Input required')])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[validators.DataRequired(message=u'Input required')])
+    password = PasswordField('Password', validators=[validators.DataRequired(message=u'Input required')])
+    remember_me = BooleanField('Remember Me')
 
 
 class All(Resource):
@@ -196,10 +219,25 @@ class CloseCSV(Resource):
         return closures
 
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_entry_cursor = users.find({"_id": int(user_id)})
+    if not db_entry_cursor.count():
+        return None
+
+    return User(user_id)  #TODO User(user_id) ?
+
+
 def hash_password(password):
     return pwd_context.encrypt(password)
 
-@auth.verify_password
+#@verify_password  #TODO
 def verify_password(username, password):
     db_entry_cursor = users.find({"username": username})
     if not db_entry_cursor.count():  # If the user is not in the database, return unauthorized
@@ -236,48 +274,69 @@ def verify_auth_token(token):
     return "Success"
 
 
-@app.route("/api/register/page")
-def register_page():
-    return flask.render_template("register.html")
+@app.route("/api/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember_me = request.form.get('remember_me')
+        if verify_password(username, password):
+            db_entry_cursor = users.find({"username": username})
+            db_entry = db_entry_cursor[0]
+            user_id = db_entry['_id']
+            session['user_id'] = user_id  # So get_token function has access to the user id for making the token
+            user = User(user_id)
+            flask_login.login_user(user, remember=remember_me)
+            flask.flash("Logged in successfully")
+
+            next = flask.request.args.get('next')
+            if not is_safe_url(next):  #TODO
+                return flask.abort(400)
+
+            return flask.redirect(next or flask.url_for('get_token'))
+    return flask.render_template('login.html', form=form)
 
 
-@app.route("/api/register", methods=['POST'])
+@app.route("/api/logout") #TODO also, restrict methods?
+def logout():
+    flask_login.logout_user()
+    return "logged out"
+
+
+@app.route("/api/register", methods=['GET', 'POST'])
 def register():
     global current_id
     taken_names = users.distinct("username")  # Get taken usernames from the database
 
-    username = request.form.get("username")
-    password = request.form.get("password")
-    if (username == None) or (password == None):
-        return 'Username and password must be provided', 400
-    if (username in taken_names) or (':' in username) or (':' in password):  # If the username is taken, username or password is blank, or either contains a ':' (seperator for http basic auth) return bad request
-        return "Bad username (taken or contains ':')", 400
+    form = RegistrationForm()
+    if form.validate_on_submit():
 
-    pass_hash = hash_password(password)
-    password = None  # Discard password
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if (username == None) or (password == None):
+            return 'Username and password must be provided', 400
+        if (username in taken_names) or (':' in username) or (':' in password):  # If the username is taken, username or password is blank, or either contains a ':' (seperator for http basic auth) return bad request
+            return "Bad username (taken or contains ':')", 400
 
-    new_user = {"_id": current_id, "username": username, "password": pass_hash}
-    users.insert_one(new_user)
+        pass_hash = hash_password(password)
+        password = None  # Discard password
 
-    response = {"Location": current_id, "username": username, "password": pass_hash}
+        new_user = {"_id": current_id, "username": username, "password": pass_hash}
+        users.insert_one(new_user)
 
-    current_id += 1
+        response = {"Location": current_id, "username": username, "password": pass_hash}
 
-    return flask.jsonify(result=response), 201
+        current_id += 1
 
+        return flask.jsonify(result=response), 201
+    return flask.render_template('register.html', form=form)
 
-@app.route("/api/token/page")
-def token_page():
-    return flask.render_template("token.html")
 
 @app.route("/api/token", methods=['GET'])
-@auth.login_required
+@flask_login.login_required
 def get_token():
-    username = request.authorization.username  # get the username, then the db entry for the user
-    db_entry_cursor = users.find({"username": username})
-    db_entry = db_entry_cursor[0]
-
-    user_id = db_entry["_id"]
+    user_id = session.get('user_id')
     token_duration_pair = generate_auth_token(user_id)
     token = token_duration_pair['token']
     duration = token_duration_pair['duration']
@@ -285,6 +344,7 @@ def get_token():
     token_str = str(token)[2:-1]  # Need to chop off b' and beginning and ' at end
     response = {'token': token_str, 'duration': duration}
     return flask.jsonify(result=response)
+    return "Login_required worked" #TODO
 
 
 # Create routes
